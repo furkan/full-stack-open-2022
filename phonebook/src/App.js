70b3b0e65a344fd27a0ef5816c98a4a0c141e832
furkan <furkanaldemir@gmail.com>
2022-10-07 17:09:26 +0300

    ADD: phonebook step3

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -2,21 +2,28 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    {
+      name: 'Arto Hellas',
+      number: '+905556667788'
+    }
   ])
   const [newName, setNewName] = useState('New person')
+  const [newNumber, setNewNumber] = useState('+90')
 
   const handleClick = (event) => {
     event.preventDefault()
     if (persons.map((person) => person.name).includes(newName)) {
       alert(`${newName} is already in the phonebook!`)
     } else {
-    setPersons(persons.concat({name: newName}))
+    setPersons(persons.concat({name: newName, number: newNumber}))
     }
   }
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -31,12 +38,19 @@ const App = () => {
           />
         </div>
         <div>
+          number:
+          <input
+            value={newNumber}
+            onChange={handleNumberChange}
+          />
+        </div>
+        <div>
           <button type="submit" onClick={handleClick}>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map((person) => <li key={person.name}>{person.name}</li>)}
+        {persons.map((person) => <li key={person.name}>{person.name}  {person.number}</li>)}
       </ul>
     </div>
   )
