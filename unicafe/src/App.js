f0f1e870821e2e2cc9486e38b5740399b34ee6cf
furkan <furkanaldemir@gmail.com>
2022-10-07 12:05:10 +0300

    ADD: unicafe part 1

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -1,14 +1,42 @@
 import { useState } from 'react'
 
+const Button = ({handleClick, text}) => <button onClick={handleClick}>{text}</button>
+
+const Feedback = (props) => (
+  <div>
+    <p>give feedback</p>
+    <Button handleClick={props.increaseGood} text='good' />
+    <Button handleClick={props.increaseNeutral} text='neutral' />
+    <Button handleClick={props.increaseBad} text='bad' />
+  </div>
+)
+
+const Statistics = (props) => (
+  <div>
+    <h2>statistics</h2>
+    <p>good {props.good}</p>
+    <p>neutral {props.neutral}</p>
+    <p>bad {props.bad}</p>
+  </div>
+)
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const increaseStat = (stat, setStat) => {
+    return () => {
+      console.log(stat, 'incremented')
+      setStat(stat + 1)
+    }
+  }
+
   return (
     <div>
-      code here
+      <Feedback increaseGood={increaseStat(good, setGood)} increaseNeutral={increaseStat(neutral, setNeutral)} increaseBad={increaseStat(bad, setBad)} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
